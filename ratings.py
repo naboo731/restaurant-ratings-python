6c3c0d8432d5d65cc272086114372441476ea7bb
Nichole Boothman <nbooth731@gmail.com>
2022-01-12 16:29:31 -0500

    complete

--- ratings.py
+++ ratings.py
@@ -1,4 +1,34 @@
 """Restaurant rating lister."""
 
 
-# put your code here
+def reading_scores():
+    restaurant_ratings = open("scores.txt")
+    scores = {}
+
+    for line in restaurant_ratings:
+        line = line.rstrip()
+        restaurant, score = line.split(":")
+        scores[restaurant] = int(score)
+
+    return scores
+
+
+def adding_new_restaurant_rating(scores):
+    print('Hello and welcome to the restaurant rating tool')
+    print('Please give type the name of the restaurant')
+    restaurant = input('(type restaurant name)')
+
+    print('Please give this restaurant a rating')
+    rating = input('type restaurant rating')
+
+    scores[restaurant] = rating
+
+
+def printed_scores(scores):
+    for restaurant, rating in sorted(scores.items()):
+        print(f'{restaurant} has a rating of {rating}')
+
+
+scores = reading_scores()
+adding_new_restaurant_rating(scores)
+printed_scores(scores)
